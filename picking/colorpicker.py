fa703f2c7caa2d3bef49b49402c5cc3d6e952616
Simon Mihevc <simon.mihevc@gmail.com>
2016-08-03 11:57:18 +0200

    added basic bounding box selection

--- picking/colorpicker.py
+++ picking/colorpicker.py
@@ -7,19 +7,35 @@ import random
 import pygame
 from pygame.locals import QUIT
 import util
-from shader import compile_program
 
 class PickableSmiley(object):
 
     def __init__(self, screen, pos):
         self.screen = screen
         self.pos = pos
+        self.selected = False
         self.img = pygame.image.load(
             os.path.join('./assets/images/picking', 'smile.png')
         )
 
     def draw(self, elapsed):
         self.screen.blit(self.img, self.pos)
+
+    def bounding_box(self):
+        rect = self.img.get_rect().size
+        return self.pos[0], self.pos[1], rect[0], rect[1]
+
+    def within_bounds(self, pos):
+        x, y, w, h = self.bounding_box()
+        if pos[0] >= x and pos[0] <= x + w:
+            pass
+        else:
+            return False
+        if pos[1] >= y and pos[1] <= y + h:
+            pass
+        else:
+            return False
+        return True
 
 
 class ColorPicker(object):
@@ -32,21 +48,6 @@ class ColorPicker(object):
         )
         self.smileys = [PickableSmiley(screen, (random.randint(100, 600), random.randint(100, 600))) for x in range(5)]
 
-        program = compile_program('''
-// Vertex program
-varying vec3 pos;
-void main() {
-    pos = gl_Vertex.xyz;
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-}
-            ''', '''
-// Fragment program
-varying vec3 pos;
-void main() {
-    gl_FragColor.rgb = pos.xyz;
-}
-            ''')
-
     def _load_img(self, full_path):
         image = pygame.image.load(full_path)
         return pygame.transform.scale(image, (800, 600))
@@ -57,8 +58,17 @@ void main() {
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 util.switch(util.Displays.MENU)
+        if event.type == pygame.MOUSEBUTTONUP:
+            loc = pygame.mouse.get_pos()
+            for smiley in self.smileys:
+                smiley.selected = smiley.within_bounds(loc)
 
     def draw(self, elapsed):
         self.screen.blit(self.background, (0,0))
         for smiley in self.smileys:
+            if smiley.selected:
+                pygame.draw.rect(
+                    self.screen,
+                    (200, 200, 0),
+                    smiley.bounding_box())
             smiley.draw(elapsed)
